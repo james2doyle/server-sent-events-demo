37d7fb23178104df9fa6d2a6cd31b3e14524c7bd
James Doyle <james2doyle@gmail.com>
2019-06-18 20:44:30 -0700

    add support for reopening the connection

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('button');
+  const close = document.getElementById('close');
+  const open = document.getElementById('open');
   const output = document.getElementById('output');
-  const evtSource = new EventSource('/sse.php');
 
-  evtSource.onopen = () => {
-    output.innerHTML = '<p>Connection to server opened.</p>';
-  };
+  function createSSE() {
+    const evtSource = new EventSource('/sse.php');
 
-  evtSource.onerror = () => {
-    output.innerHTML = '<p>EventSource failed.</p>';
-  };
+    evtSource.onopen = () => {
+      output.innerHTML = '<p>Connection to server opened.</p>';
+    };
 
-  evtSource.onmessage = ({ data }) => {
-    const { time } = JSON.parse(data);
-    const formatted = new Intl.DateTimeFormat('en-CA', {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-    }).format(Date.parse(time));
-    const code = document.createElement('p');
-    code.textContent = formatted;
-    output.insertBefore(code, output.firstChild);
-  };
+    evtSource.onerror = () => {
+      output.innerHTML = '<p>EventSource failed.</p>';
+    };
 
-  button.onclick = () => {
+    evtSource.onmessage = ({ data }) => {
+      const { time } = JSON.parse(data);
+      const formatted = new Intl.DateTimeFormat('en-CA', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      }).format(Date.parse(time));
+      const code = document.createElement('p');
+      code.textContent = formatted;
+      output.insertBefore(code, output.firstChild);
+    };
+
+    return evtSource;
+  }
+
+  let evtSource = createSSE();
+
+  close.onclick = () => {
     evtSource.close();
     output.innerHTML = '<p>Connection to server closed.</p>';
+    open.style.display = 'initial';
+    close.style.display = 'none';
+  };
+
+  open.onclick = () => {
+    close.style.display = 'initial';
+    open.style.display = 'none';
+    evtSource = createSSE();
   };
 });
